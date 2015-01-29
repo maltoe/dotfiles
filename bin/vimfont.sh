89be00b43f6e171ce9224c997b50ca958b80b774
Malte Rohde <malte.rohde@flavoursys.com>
2015-01-29 17:03:54 +0100

    Add big option vimfont.sh

--- bin/vimfont.sh
+++ bin/vimfont.sh
@@ -2,6 +2,8 @@
 
 if [ "x$1" = "xreset" ]; then
   printf "\33]50;xft:Inconsolata:antialias=true:hinting=true:pixelsize=18\007"
+elif [ "x$1" = "xbig" ]; then
+  printf "\33]50;xft:Inconsolata:antialias=true:hinting=true:pixelsize=24\007"
 else
   printf "\33]50;xft:Inconsolata:bold:antialias=true:hinting=true:pixelsize=14\007"
 fi
