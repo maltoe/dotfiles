e4a746c812219e5a19c016ddd7f4e2a688ace132
Malte Rohde <malte.rohde@plugintheworld.com>
2016-05-20 14:16:23 +0200

    vimfont.sh: Disable if on second screen.

--- bin/vimfont.sh
+++ bin/vimfont.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SCREEN=$(echo 'return client.focus.screen' | awesome-client | xargs | cut -d ' ' -f 2)
+if [ $? == 0 ] && [ "$SCREEN" == "2" ]; then
+  exit
+fi
+
 if [ "x$1" = "xreset" ]; then
   printf "\33]50;xft:Inconsolata:antialias=true:hinting=true:pixelsize=18\007"
 elif [ "x$1" = "xbig" ]; then
