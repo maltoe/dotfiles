3dfad471ffbb9946982e5d42590f024f7677f58d
Malte Rohde <malte.rohde@plugintheworld.com>
2017-06-19 17:25:09 +0200

    add redshift config, adjust smooth backlight to use xrandr

--- bin/smooth_backlight.rb
+++ bin/smooth_backlight.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-steps       = [1, 5, 10, 15, 20, 30, 45, 70, 100]
-current     = `xbacklight -get`.to_f
+steps       = [0.01, 0.05, 0.10, 0.15, 0.20, 0.30, 0.45, 0.70, 1.0]
+current     = /\d\.\d+/.match(`xrandr --verbose | grep Brightness | head -n 1`.strip)[0].to_f
 closest_idx = steps.map { |s| (s - current).abs }.each_with_index.min.last
 direction   = ARGV.first == 'up' ? 1 : -1
 nextstep    = [[closest_idx + direction, 0].max, steps.size - 1].min
 
-system "xbacklight -set #{steps[nextstep]}" if nextstep != closest_idx
+system "xrandr --output eDP-1 --brightness #{steps[nextstep]}" if nextstep != closest_idx
