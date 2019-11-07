59e3d83587b41725921c8d3d753041736c12cdcf
Malte Rohde <malte@bitcrowd.net>
2019-11-07 16:24:37 +0100

    update

--- bin/docker-clean-up.sh
+++ bin/docker-clean-up.sh
@@ -3,3 +3,4 @@
 docker ps -a | grep 'Exited' | awk '{print $1}' | xargs docker rm
 docker images | grep '^<none>' | awk '{print $3}' | xargs docker rmi
 docker volume ls -qf dangling=true | xargs docker volume rm
+docker system prune
